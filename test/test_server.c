5f0372106d0c159762a5ca0c40c4f362dcddc015
Clay Dowling <clay@lazarusid.com>
2017-09-17 13:54:39 -0400

    Added test fixture

--- test/test_server.c
+++ test/test_server.c
@@ -7,8 +7,13 @@
 #define TEST_PORT 5150
 #define SOCKET_FD 27
 
+void setup(void) { socket_will_return(SOCKET_FD); }
+
+void teardown(void) {
+    // Do Nothing
+}
+
 START_TEST(makeSocket_byDefault_createsInternetStreamSocket) {
-    socket_will_return(SOCKET_FD);
     make_socket(TEST_PORT);
 
     ck_assert_int_eq(PF_INET, socket_called_with_domain());
@@ -17,7 +22,6 @@ START_TEST(makeSocket_byDefault_createsInternetStreamSocket) {
 END_TEST
 
 START_TEST(makeSocket_byDefault_returnsFdFromSocket) {
-    socket_will_return(SOCKET_FD);
     ck_assert_int_eq(SOCKET_FD, make_socket(TEST_PORT));
 }
 END_TEST
@@ -32,6 +36,8 @@ TCase *tcase_server(void) {
     TCase *tc;
 
     tc = tcase_create("server");
+    tcase_add_checked_fixture(tc, setup, teardown);
+
     tcase_add_test(tc, makeSocket_byDefault_createsInternetStreamSocket);
     tcase_add_test(tc, makeSocket_byDefault_returnsFdFromSocket);
     tcase_add_exit_test(tc, makeSocket_onError_ExitsWithError, EXIT_FAILURE);
